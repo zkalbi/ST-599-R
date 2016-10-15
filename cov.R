1bbe6602f64986ff80c6f3fc87c0fcdc9d333f27
Kalbi ZONGO <zkalbi@gmail.com>
2016-10-14 22:10:16 -0700

    corrected the cov definition

--- cov.R
+++ cov.R
@@ -1,7 +1,10 @@
 
+
+# this function compute the covariance of a numeric vector
 COV <- function(x){
   x <- x[!is.na(x)] # remove na
-  return(mean(x)/sd(x))
+ # return(mean(x)/sd(x))      : wrong
+  return(sd(x)/mean(x))
 }
 
 samp <- runif(100, 0,100)
